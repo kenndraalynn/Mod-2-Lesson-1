599510c8f8eb547a006686d0d217d0f0d5d38c45
kenndraalynn <kenndraalynn@gmail.com>
2024-12-14 01:47:16 -0500

    Initial commit

---
+++ 1.showcasedancemoves.py
@@ -0,0 +1,14 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+mood = input("How do you feel today? happy or sad  ")
+
+if mood == "happy":
+    print("That's great to hear!")
+else: 
+    print("I hope your day gets better!")
---
+++ 3.arithmeticoperations.py
@@ -0,0 +1,10 @@
+eggs = 3 
+bread = 5 
+milk = 8
+
+total = eggs + bread + milk
+
+print(total)
+
+
+ban
